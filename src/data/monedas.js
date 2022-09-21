9a4261e8bf7d1b1e73fd7ab18ecb69787fe0c087
sadelto0130 <vsalaz00@live.com>
2022-09-21 00:51:42 -0300

    Proyecto cotizador cirpto finalizado

---
+++ src/data/monedas.js
@@ -0,0 +1,10 @@
+const monedas = [
+  {id: 'USD', nombre: 'Dolar USA'},
+  {id: 'MXN', nombre: 'Peso Mexicano'},
+  {id: 'COP', nombre: 'Peso Colombiano'},
+  {id: 'ARS', nombre: 'Peso Argentino'},
+]
+
+export {
+  monedas
+}
